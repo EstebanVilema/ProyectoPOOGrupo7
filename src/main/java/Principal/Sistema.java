fdd8913515d2a66a358cd90da9d6c669d7c1063a
EstebanVilema <esvilema@espol.edu.ec>
2022-07-09 13:05:37 -0500

    cambio de nombre en clases y paquetes

--- src/main/java/Principal/Sistema.java
+++ src/main/java/Principal/Sistema.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package sistema;
+package Principal;
 
 import java.util.ArrayList;
 
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  *
  * @author usuario
  */
-public class sistema {
+public class Sistema {
     //private ArrayList<listaCliente> clientes;
     //private ArrayList<listaServicios> servicio;
     
